4ba027b751fad346f1dacc2339847cdd103e51c6
bit <bit@DESKTOP-0G705V6>
2018-12-28 16:33:13 +0900

    문제 해결

--- src/prob4/StringUtil.java
+++ src/prob4/StringUtil.java
@@ -1,5 +1,16 @@
 package prob4;
 
-public class StringUtil {
+public class StringUtil 
+{
+	public static String concatenate(String[] str) 
+	{
+        //문자열을 결합하여 리턴 하는 메소드 구현
+		StringBuilder strBuilder = new StringBuilder();
+    	
+    	for (int i = 0; i < str.length; i++)
+    		strBuilder.append(str[i]);
+    	
+    	return strBuilder.toString();
+    }
 
 }
