b7208a676943585c5e5d280a5874b4ca94030d5d
bit <bit@DESKTOP-0G705V6>
2018-12-28 16:37:58 +0900

    문제 해결

--- src/prob5/Account.java
+++ src/prob5/Account.java
@@ -1,4 +1,40 @@
 package prob5;
 
-public class Account {
+public class Account 
+{
+	private String accountNo;
+	private int balance;
+	
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public Account(String accountNo)
+	{
+		this.accountNo = accountNo;
+	}
+
+	public String getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+	
+	public void save(int saveMoney)
+	{
+		balance += saveMoney;
+	}
+	
+	public void deposit(int depositMoney)
+	{
+		balance -= depositMoney;
+	}
+	
+	
 }
