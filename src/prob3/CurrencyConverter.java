7d6a7baf0a4303f4c4cf65cd473ba8bd14776b1b
bit <bit@DESKTOP-0G705V6>
2018-12-28 18:55:17 +0900

    문제 해결

--- src/prob3/CurrencyConverter.java
+++ src/prob3/CurrencyConverter.java
@@ -1,4 +1,27 @@
 package prob3;
 
-public class CurrencyConverter {
+public class CurrencyConverter 
+{
+	private static double rate;
+	private static int dollar;
+	private static int krw;
+	
+	public static void setRate(double value) 
+	{
+		rate = value;
+	}
+	
+	public static int toDollar(int value)
+	{
+		dollar = value;
+		return dollar;
+	}
+	
+	public static int toKRW(int value)
+	{
+		krw = value;
+		return krw;
+	}
+	
+	
 }
